20d8730655368cdb9c435ebac228989395e39067
cdias1374 <cdias1374@gmail.com>
2025-03-07 18:31:32 +0000

    CRIEI PRINTF CRIEI JOGO CRIEI VALORES JOGO QUASE PRONTO

---
+++ super_trunfo/super_trunfo.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+
+int main(){
+
+              // CARTA1
+
+printf("BEM VINDO AO SUPER TRUNFO:\n");
+printf("CADSTRE SUAS CARTAS E BOM JOGO:\n");
+
+char estado1, estado2;
+char codigocidade1, codigocidade2;
+char nomecidade1[20], nomecidade2[20];
+int populacao1, populacao2;
+float area1, area2;
+float pib1, pib2;
+int pontosturristico1, pontosturristico2;
+
+printf("digite estado1:\n");
+scanf("%s", &estado1);
+
+printf("digite codigocidade1:\n");
+scanf("%s", &codigocidade1);
+
+printf("digite nomecidade1:\n");
+scanf("%s", &nomecidade1);
+
+printf("digite populacao1:\n");
+scanf("%d", &populacao1);
+
+printf("digite area1:\n");
+scanf("%f", &area1);
+
+printf("digite o pib1:\n");
+scanf("%f", &pib1);
+
+printf("digite pontosturristico1:\n");
+scanf("%d", &pontosturristico1);
+
+              //CARTA 2
+        
+              printf("digite estado2:\n");
+              scanf("%s", &estado2);
+              
+              printf("digite codigocidade2:\n");
+              scanf("%s", &codigocidade2);
+              
+              printf("digite nomecidade2:\n");
+              scanf("%s", &nomecidade2);
+              
+              printf("digite populacao2:\n");
+              scanf("%d", &populacao2);
+              
+              printf("digite area2:\n");
+              scanf("%f", &area2);
+              
+              printf("digite o pib2:\n");
+              scanf("%f", &pib2);
+              
+              printf("digite pontosturristico2:\n");
+              scanf("%d", &pontosturristico2);
+              
+
+}
